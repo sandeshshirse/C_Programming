6f395422cb550d454545a34d5604736232deb83a
sandeshshirse <shirse170906@gmail.com>
2025-10-30 00:17:35 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,15 @@
+
+let myName;
+
+myName = "student's name"; 
+
+
+let greetingMessage;
+
+
+greetingMessage = "Hello, ";
+greetingMessage = greetingMessage + myName;
+greetingMessage = greetingMessage + "! Welcome to JavaScript!";
+
+
+console.log(greetingMessage);
