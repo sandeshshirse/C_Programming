11b36d4497ac9ceec42226b956a57fed76a6294a
Sandesh Shirse <shirse170906@gmail.com>
2025-11-04 23:33:10 +0530

    Add files via upload

---
+++ cc_homework/a012.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float length, width, area;
+
+   
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length); 
+
+    
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &width); 
+
+    
+    area = length * width;
+
+   
+    printf("\nThe area of the rectangle is: %.2f\n", area);
+
+    return 0;
+}
