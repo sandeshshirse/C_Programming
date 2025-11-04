8c364b137233c802357ea9fd6854b93e2e548267
Sandesh Shirse <shirse170906@gmail.com>
2025-11-04 23:53:44 +0530

    Add files via upload

---
+++ cc_homework/a014.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float score;
+    
+    
+    printf("Enter Math score (0-100): ");
+    scanf("%f", &score); 
+    
+    printf("Grade: ");
+
+    
+    if (score >= 90) {
+        printf("A\n");
+    } else if (score >= 80) {
+        printf("B\n");
+    } else if (score >= 70) {
+        printf("C\n");
+    } else if (score >= 60) {
+        printf("D\n");
+    } else {
+        printf("F\n");
+    }
+    
+    return 0;
+}
