a5de63e10734fe850b7474654b08930b28df8442
Sandesh Shirse <shirse170906@gmail.com>
2026-01-27 18:47:41 +0530

    Add name input and output functionality

---
+++ cc_homework/name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    char fname[20], lname[20];
+    
+    printf("what is your first name :");
+    scanf("%s", fname);
+    
+    printf("what is your last name :");
+    scanf("%s", lname);
+    
+    
+    
+    printf ("you full name is %s %s\n", fname, lname);
+}
